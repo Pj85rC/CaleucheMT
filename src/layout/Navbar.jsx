83889510a05c9a65b2733c5154f414bcdfa57816
Pj85rC <patricio.ramirez.c@gmail.com>
2023-07-05 23:56:16 -0400

    [C6] Fondo global (Background Color)

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -3,21 +3,21 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <AppBar position="fixed">
+    <AppBar position="fixed" color="transparent" sx={{ backgroundColor: '#F9F9F9', color:'#09071D' }}>
       <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Typography variant="h6" component={NavLink} to="/" sx={{ flexGrow: 1 }}>
           Caleuche
         </Typography>
         <Button component={NavLink} to="/" color="inherit">
           Home
         </Button>
-        <Button component={NavLink} to="/festivales" color="inherit">
+        <Button component={NavLink} to="/festivales" >
           Festivales
         </Button>
-        <Button component={NavLink} to="/quienesSomos" color="inherit">
+        <Button component={NavLink} to="/quienesSomos" >
           Quienes Somos
         </Button>
-        <Button component={NavLink} to="/contactanos" color="inherit">
+        <Button component={NavLink} to="/contactanos" >
           Contáctanos
         </Button>
       </Toolbar>
