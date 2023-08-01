fcfeaa8c3216177cafc0e97103185f051ddf5c75
Dirk Gaston <dirkgaston87@gmail.com>
2023-08-01 08:13:06 -0400

    [C35] Vista Contáctanos

--- src/layout/Content.jsx
+++ src/layout/Content.jsx
@@ -3,7 +3,7 @@ import { Home } from "../views/Home";
 import { Login } from "../views/Login";
 import { Registro } from "../views/Registro";
 import { AboutUs } from "../views/QuienesSomos";
-import { Contactanos } from "../views/Contactanos";
+import { Contact } from "../views/Contactanos";
 import { PerfilUsuario } from "../views/PerfilUsuario";
 import { Festivales } from "../views/Festivales";
 import { DetalleFestivales } from "../views/DetalleFestivales";
@@ -18,7 +18,7 @@ export const Content = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/registro" element={<Registro />} />
           <Route path="/quienesSomos" element={<AboutUs />} />
-          <Route path="/contactanos" element={<Contactanos />} />
+          <Route path="/contactanos" element={<Contact />} />
           <Route path="/perfilUsuario" element={<PerfilUsuario />} />
           <Route path="/festivales" element={<Festivales />} />
           <Route path="/detalleFestivales" element={<DetalleFestivales />} />
--- src/views/Contactanos.jsx
+++ src/views/Contactanos.jsx
@@ -1,8 +1,69 @@
-export const Contactanos = () => {
-    return (
-      <>
-        <h1>Contáctanos</h1>
-      </>
-    );
-  };
-  
+import { Box, Container, Typography } from "@mui/material";
+import PhoneIcon from "@mui/icons-material/Phone";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import EmailIcon from "@mui/icons-material/Email";
+import { useTheme } from "@emotion/react";
+
+export const Contact = () => {
+  const theme = useTheme();
+
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        pt: "70px",
+      }}
+    >
+      <Container
+        maxWidth="xs"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h1 style={{ color: theme.palette.primary.main, fontSize: "2.5rem" }}>
+          CONTÁCTANOS
+        </h1>
+        <Typography align="center" paragraph color={theme.palette.primary.main}>
+          Para cualquier consulta, no dudes en contactarnos a través de los
+          siguientes medios:
+        </Typography>
+        <Box sx={{ display: "flex", alignItems: "center", my: 2 }}>
+          <PhoneIcon color="primary" />
+          <Typography
+            variant="body1"
+            color={theme.palette.primary.main}
+            sx={{ ml: 1 }}
+          >
+            +123 456 7890
+          </Typography>
+        </Box>
+        <Box sx={{ display: "flex", alignItems: "center", my: 2 }}>
+          <WhatsAppIcon color="primary" />
+          <Typography
+            variant="body1"
+            color={theme.palette.primary.main}
+            sx={{ ml: 1 }}
+          >
+            +123 456 7890
+          </Typography>
+        </Box>
+        <Box sx={{ display: "flex", alignItems: "center", my: 2 }}>
+          <EmailIcon color="primary" />
+          <Typography
+            variant="body1"
+            color={theme.palette.primary.main}
+            sx={{ ml: 1 }}
+          >
+            example@example.com
+          </Typography>
+        </Box>
+      </Container>
+    </Box>
+  );
+};
