cd1e4abe52f64c5eee216bf8ca6f24f97574f2a9
Pj85rC <patricio.ramirez.c@gmail.com>
2023-08-12 07:49:34 -0400

    fix(Content): Adding routes

--- src/layout/Content.jsx
+++ src/layout/Content.jsx
@@ -5,26 +5,69 @@ import { Registro } from "../views/auth/Registro";
 import { AboutUs } from "../views/QuienesSomos";
 import { Contact } from "../views/Contactanos";
 import { PerfilUsuario } from "../views/PerfilUsuario";
-import { Festivales } from "../views/Festivals/Festivales";
-import { DetalleFestivales } from "../views/Festivals/DetalleFestivales";
-import { AddArtist } from '../views/admin/AddArtist'
-
-
+import { Festivales } from "../views/festivals/Festivales";
+import { DetalleFestivales } from "../views/festivals/DetalleFestivales";
+import { AddArtist } from "../views/admin/AddArtist";
+import { AdminMenu } from "../views/admin/AdminMenu";
+import { AddFestival } from "../views/admin/AddFestival";
+import RouteGuard from "./RouteGuard";
 
 export const Content = () => {
-    return (
-      <>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/registro" element={<Registro />} />
-          <Route path="/quienesSomos" element={<AboutUs />} />
-          <Route path="/contactanos" element={<Contact />} />
-          <Route path="/perfilUsuario" element={<PerfilUsuario />} />
-          <Route path="/festivales" element={<Festivales />} />
-          <Route path="/festival/:id" element={<DetalleFestivales />} />
-          <Route path="/admin/artists" element={<AddArtist />} />
-        </Routes>
-      </>
-    );
-  };
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registro" element={<Registro />} />
+        <Route path="/quienesSomos" element={<AboutUs />} />
+        <Route path="/contactanos" element={<Contact />} />
+        <Route
+          path="/profile"
+          element={
+            <RouteGuard requiredRoles={["user"]}>
+              <PerfilUsuario />
+            </RouteGuard>
+          }
+        />
+
+        <Route path="/festivales" element={<Festivales />} />
+
+        <Route
+          path="/festival/:id"
+          element={
+            <RouteGuard requiredRoles={["admin", "user"]}>
+              <DetalleFestivales />
+            </RouteGuard>
+          }
+        />
+
+        <Route
+          path="/admin"
+          element={
+            <RouteGuard requiredRoles={["admin"]}>
+              <AdminMenu />
+            </RouteGuard>
+          }
+        />
+
+        <Route
+          path="/admin/artists"
+          element={
+            <RouteGuard requiredRoles={["admin"]}>
+              <AddArtist />
+            </RouteGuard>
+          }
+        />
+
+        <Route
+          path="/admin/festivals"
+          element={
+            <RouteGuard requiredRoles={["admin"]}>
+              <AddFestival />
+            </RouteGuard>
+          }
+        />
+      </Routes>
+    </>
+  );
+};
