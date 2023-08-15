1ba3374c3031a7bec33204217f74f3496efca87c
Pj85rC <patricio.ramirez.c@gmail.com>
2023-08-14 23:28:13 -0400

    feat(FestivalCard): add favorite functionality

--- src/components/Cards/FestivalCard.jsx
+++ src/components/Cards/FestivalCard.jsx
@@ -1,3 +1,4 @@
+import { useState, useContext, useEffect } from "react";
 import {
   Box,
   Card,
@@ -6,16 +7,34 @@ import {
   CardMedia,
   Typography,
   useTheme,
+  IconButton,
 } from "@mui/material";
 import { CustomButton } from "../CustomButton";
+import { AuthContext } from "../../context/AuthContext";
+import { FavoritesContext } from "../../context/FavContext";
+import { addFavorite, removeFavorite } from "../../api/favorites";
+
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 export const FestivalCard = ({
+  id,
   title,
   imageUrl,
   description,
   onDetailClick,
 }) => {
   const theme = useTheme();
+  const { user } = useContext(AuthContext);
+  const { favorites, setFavorites } = useContext(FavoritesContext);
+
+  const [favorited, setFavorited] = useState(false);
+
+  useEffect(() => {
+    setFavorited(favorites.some((fav) => fav.festival_id === id));
+  }, [favorites, id]);
+
+  const userId = user.userId;
 
   return (
     <Card
@@ -35,6 +54,27 @@ export const FestivalCard = ({
         }}
       >
         <CardMedia component="img" height="250" image={imageUrl} alt={title} />
+        <IconButton
+          onClick={(e) => {
+            e.stopPropagation();
+            if (favorited) {
+              removeFavorite(id, userId, favorites, setFavorites);
+            } else {
+              addFavorite(id, userId, favorites, setFavorites);
+            }
+            setFavorited(!favorited);
+          }}
+          sx={{ position: "absolute", top: "45px", right: "45px" }}
+        >
+          {favorited ? (
+            <FavoriteIcon color="error" sx={{ strokeWidth: 2 }} />
+          ) : (
+            <FavoriteBorderIcon
+              color="white"
+              sx={{ strokeWidth: 2, color: "white" }}
+            />
+          )}
+        </IconButton>
         <CardContent sx={{ color: theme.palette.text.card }}>
           <Typography gutterBottom variant="h5" component="div">
             {title}
